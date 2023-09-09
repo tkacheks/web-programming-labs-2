2bebb50b573a60aeed0c5cae87ced1b4796c34f1
Давоян Карина <tkacheks03@mail.ru>
2023-09-09 15:48:04 +0700

    первое приложение

--- app.py
+++ app.py
@@ -3,5 +3,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
-    
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title> Давоян Карине Горовна, лабораторная 1 </title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Давоян Карине, ФБИ-11, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+
+
+
+   
+
+
+
+
+
