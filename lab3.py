608faf8b69057a5284625d9fd4fcab822ab36b74
Давоян Карина <tkacheks03@mail.ru>
2023-10-07 19:08:27 +0700

    заказ напитка

--- lab3.py
+++ lab3.py
@@ -19,3 +19,32 @@ def form1():
     
     sex = request.args.get('sex')
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors, errors2=errors2) 
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price=0
+    drink=request.args.get('drink')
+# Пусть кофе стоит 120, черный чай 80, зеленый чай 70
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+#Добавка молока удорожает напиток на 30, а сахара на 10.
+    if request.args.get('milk') == 'on':
+        price +=30
+    if request.args.get('sugar') == 'on':
+        price +=10
+    return render_template('pay.html', price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('success.html')
