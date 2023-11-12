3dfe2eb57384ae05501d0d9675b8e2121aba9380
Давоян Карина <tkacheks03@mail.ru>
2023-11-12 15:16:07 +0700

    заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 
 
