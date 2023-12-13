1de50dbbd25dce5003bf9da718cf05831afa60cf
Давоян Карина <tkacheks03@mail.ru>
2023-12-13 22:07:32 +0700

    Удаление выбранного курса по REST

--- lab8.py
+++ lab8.py
@@ -21,3 +21,11 @@ def get_course(course_num):
         abort(404)
     return courses[course_num]
 
+@lab8.route('/lab8/api/courses/<int:course_num>',methods=['DELETE'])
+def del_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        abort(404)
+    del courses[course_num]
+    return '', 204
+
+
