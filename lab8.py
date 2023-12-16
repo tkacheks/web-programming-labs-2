2670f874d6325ea8f9da488ce77ae59bd7bceeb7
Давоян Карина <tkacheks03@mail.ru>
2023-12-16 14:57:26 +0700

    редактирование выбранного курса по rest

--- lab8.py
+++ lab8.py
@@ -28,4 +28,12 @@ def del_course(course_num):
     del courses[course_num]
     return '', 204
 
+@lab8.route('/lab8/api/courses/<int:course_num>',methods=['PUT'])
+def put_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        abort(404)
+    course = request.get_json()
+    courses[course_num] = course
+    return courses[course_num]
+
 
