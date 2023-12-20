7a5cc2feaeaef03785f4149c8a00b8f65a65da36
Давоян Карина <tkacheks03@mail.ru>
2023-12-20 23:11:08 +0700

    Обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template, request, abort
+from flask import Blueprint, render_template, request, abort,jsonify
 lab9 = Blueprint('lab9', __name__)
 
 
 @lab9.route('/lab9/')
 def main():
     return render_template('lab9/index.html')
+
+
+@lab9.app_errorhandler(404)
+def not_found(e):
+    return "нет такой страницы",404
+
+
+@lab9.app_errorhandler(500)
+def error500(err):
+    return render_template('500.html')
+
+
+@lab9.route('/lab9/500')
+def error():
+    result = 1 / 0
+    return result
